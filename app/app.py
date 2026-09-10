cfcddbac8b6f01659ae4bc81a6f44de852043eee
Mable angelina <24bscit027.cas@ppg.edu.in>
2026-09-10 10:20:59 +0530

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app=flask(_name_)
+@app.route("/")
+def home():
+  return "hello from Docker! My first container is running."
+  if_name_=="_main_":
+  app.run(host="0.0.0.0",post=5000)
+  
